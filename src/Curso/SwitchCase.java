d3abbc895f73863dae0cf57fac9be6c6bdbd3361
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2019-05-28 18:30:05 -0300

    SwitchCase

---
+++ src/Curso/SwitchCase.java
@@ -0,0 +1,51 @@
+package Curso;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		String dia;
+		
+		switch(x) {
+		
+		case 1:
+			dia = "Domingo";
+			break;
+		case 2:
+			dia = "Segunda";
+			break;
+		case 3:
+			dia = "Terça";
+			break;	
+		case 4:
+			dia = "Quarta";
+			break;
+		case 5:
+			dia = "Quinta";
+			break;
+		case 6:
+			dia = "Sexta";
+			break;
+		case 7:
+			dia = "Sabado";
+			break;
+		default:
+			dia = "Valor inválido";
+		}
+		
+		System.out.println("Dia: " + dia);
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
